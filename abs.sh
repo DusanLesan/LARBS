ea1cd285f4b8fef99cf5ed5a38429d36d87e87a8
Dusan <oalfodr@gmail.com>
2022-06-05 06:28:32 +0200

    remove base configs

--- abs.sh
+++ abs.sh
@@ -15,69 +15,18 @@ nvimmanagerrepo="https://github.com/wbthomason/packer.nvim"
 nvimmanagerdir=".local/share/nvim/site/pack/packer/start/packer.nvim"
 
 ### FUNCTIONS ###
-
-basesetup() {
-	passwd
-	TZuser=$(cat tzfinal.tmp)
-	ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
-	hwclock --systohc
-	echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-	echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-	echo "en_US ISO-8859-1" >> /etc/locale.gen
-	locale-gen
-
-	pacman --noconfirm --needed -S networkmanager
-	systemctl enable NetworkManager
-	systemctl start NetworkManager
-}
-
 installpkg(){ pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 ;}
 
 error() { clear; printf "ERROR:\\n%s\\n" "$1" >&2; exit 1;}
 
-getuserandpass() {
+getuser() {
 	# Prompts user for new username an password.
 	name=$(dialog --inputbox "First, please enter a name for the user account." 10 60 3>&1 1>&2 2>&3 3>&1) || exit 1
 	while ! echo "$name" | grep -q "^[a-z_][a-z0-9_-]*$"; do
 		name=$(dialog --no-cancel --inputbox "Username not valid. Give a username beginning with a letter, with only lowercase letters, - or _." 10 60 3>&1 1>&2 2>&3 3>&1)
 	done
-	pass1=$(dialog --no-cancel --passwordbox "Enter a password for that user." 10 60 3>&1 1>&2 2>&3 3>&1)
-	pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
-	while ! [ "$pass1" = "$pass2" ]; do
-		unset pass2
-		pass1=$(dialog --no-cancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
-		pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
-	done ;
-}
-
-usercheck() {
-	! { id -u "$name" >/dev/null 2>&1; } ||
-	dialog --colors --title "WARNING!" --yes-label "CONTINUE" --no-label "No wait..." --yesno "The user \`$name\` already exists on this system. LARBS can install for a user already existing, but it will \\Zboverwrite\\Zn any conflicting settings/dotfiles on the user account.\\n\\nLARBS will \\Zbnot\\Zn overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that LARBS will change $name's password to the one you just gave." 14 70
-}
-
-preinstallmsg() {
-	dialog --title "Let's get this party started!" --yes-label "Let's go!" --no-label "No, nevermind!" --yesno "The rest of the installation will now be totally automated, so you can sit back and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press <Let's go!> and the system will begin installation!" 13 60 || { clear; exit 1; }
-}
-
-adduserandpass() {
-	# Adds user `$name` with password $pass1.
-	dialog --infobox "Adding user \"$name\"..." 4 50
-	useradd -m -g wheel -s /bin/zsh "$name" >/dev/null 2>&1 ||
-	usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
 	repodir="/home/$name/.local/src"; mkdir -p "$repodir"; chown -R "$name":wheel "$(dirname "$repodir")"
-	echo "$name:$pass1" | chpasswd
-	unset pass1 pass2 ;
 }
-
-refreshkeys() {
-	dialog --infobox "Refreshing Arch Keyring..." 4 40
-	pacman -Q artix-keyring >/dev/null 2>&1 && pacman --noconfirm -S artix-keyring >/dev/null 2>&1
-	pacman --noconfirm -S archlinux-keyring >/dev/null 2>&1
-}
-
-newperms() { # Set special sudoers settings for install (or after).
-	sed -i "/#LARBS/d" /etc/sudoers
-	echo "$* #LARBS" >> /etc/sudoers ;}
 
 manualinstall() { # Installs $1 manually if not installed. Used only for AUR helper here.
 	[ -f "/usr/bin/$1" ] || (
@@ -175,29 +124,6 @@ systembeepoff() {
 	echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;
 }
 
-bootloader() {
-	case $(dialog --title "Boot loader" --cancel-label "Skip" --menu "Select boot loader:" 7 70 0 "BIOS" "GRUB" "UEFI" "systemd-boot" 3>&1 1>&2 2>&3 3>&1) in
-		"BIOS")
-			while read -r path size; do
-				args+=("$path")
-				args+=("$size")
-			done < <(lsblk -dnrpo "name,size")
-			cmd=(dialog --keep-tite --menu "Select drive:" 22 76 16)
-			choice=$("${cmd[@]}" "${args[@]}" 2>&1 >/dev/tty)
-			[ -n "$choice" ] && pacman --noconfirm --needed -S grub && grub-install --target=i386-pc $choice && grub-mkconfig -o /boot/grub/grub.cfg ;;
-
-		"UEFI")
-			bootctl install
-			mkdir -p ~/boot/loader/entries/
-			root=$(lsblk -rpo "mountpoints,uuid" | grep -oP "^/ \K.*")
-			printf "%s\n%s\n%s\n%s" \
-				"title     Arch Lnux" \
-				"linux     /vmlinuz-linux" \
-				"initrd    /initramfs-linux.img" \
-				"options   root=PARTUUID=$root rw" > ~/boot/loader/entries/arch.conf ;;
-	esac
-}
-
 displaymanager() {
 	aurinstall ly
 	systemctl enable ly.service
@@ -205,51 +131,19 @@ displaymanager() {
 
 ### THE ACTUAL SCRIPT ###
 
-### This is how everything happens in an intuitive format and order.
-
-basesetup
-
 # Check if user is root on Arch distro. Install dialog.
 pacman --noconfirm --needed -Sy dialog || error "Are you sure you're running this as the root user, are on an Arch-based distribution and have an internet connection?"
 
-# Get and verify username and password.
-getuserandpass || error "User exited."
-
-# Give warning if user already exists.
-usercheck || error "User exited."
+# Get and verify username.
+getuser || error "User exited."
 
 # Allow user to select programs he wants
 getpackagelist 0
-
-# Last chance for user to back out before install.
-preinstallmsg || error "User exited."
-
-# Refresh Arch keyrings.
-refreshkeys || error "Error automatically refreshing Arch keyring. Consider doing so manually."
 
 for x in curl base-devel git ntp zsh; do
 	dialog --title "LARBS Installation" --infobox "Installing \`$x\` which is required to install and configure other programs." 5 70
 	installpkg "$x"
 done
-
-dialog --title "LARBS Installation" --infobox "Synchronizing system time to ensure successful and secure installation of software..." 4 70
-ntpdate 0.us.pool.ntp.org >/dev/null 2>&1
-
-adduserandpass || error "Error adding username and/or password."
-
-[ -f /etc/sudoers.pacnew ] && cp /etc/sudoers.pacnew /etc/sudoers # Just in case
-
-# Allow user to run sudo without password. Since AUR programs must be installed
-# in a fakeroot environment, this is required for all builds with AUR.
-newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
-
-# Make pacman and yay colorful and adds eye candy on the progress bar because why not.
-grep -q "^Color" /etc/pacman.conf || sed -i "s/^#Color$/Color/" /etc/pacman.conf
-grep -q "ILoveCandy" /etc/pacman.conf || sed -ir "s/#VerbosePkgLists/VerbosePkgLists\nILoveCandy/" /etc/pacman.conf
-grep -q "^ParallelDownloads" /etc/pacman.conf || sed -i "s/^#ParallelDownloads.*/ParallelDownloads = $(nproc)/" /etc/pacman.conf
-
-# Use all cores for compilation.
-sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
 
 manualinstall $aurhelper || error "Failed to install AUR helper."
 
@@ -278,26 +172,9 @@ systembeepoff
 # Install display manager
 displaymanager
 
-# Install GRUB or systemd-boot
-bootloader
-
 # Make zsh the default shell for the user.
 chsh -s /bin/zsh "$name" >/dev/null 2>&1
 sudo -u "$name" mkdir -p "/home/$name/.cache/zsh/"
-
-# Tap to click
-[ ! -f /etc/X11/xorg.conf.d/40-libinput.conf ] && printf 'Section "InputClass"
-	Identifier "libinput touchpad catchall"
-	MatchIsTouchpad "on"
-	MatchDevicePath "/dev/input/event*"
-	Driver "libinput"
-	# Enable left mouse button by tapping
-	Option "Tapping" "on"
-EndSection' > /etc/X11/xorg.conf.d/40-libinput.conf
-
-# Fix fluidsynth/pulseaudio issue.
-grep -q "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" /etc/conf.d/fluidsynth ||
-	echo "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" >> /etc/conf.d/fluidsynth
 
 # Start/restart PulseAudio.
 killall pulseaudio; sudo -u pulseaudio --start
@@ -305,9 +182,3 @@ killall pulseaudio; sudo -u pulseaudio --start
 # Remove dialog
 pacman -Rcns --noconfirm dialog
 
-# This line, overwriting the `newperms` command above will allow the user to run
-# serveral important commands, `shutdown`, `reboot`, updating, etc. without a password.
-newperms "%wheel ALL=(ALL) ALL #LARBS
-%wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin/systemctl restart NetworkManager,/usr/bin/rc-service NetworkManager restart,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/yay,/usr/bin/pacman -Syyuw --noconfirm"
-
-clear
