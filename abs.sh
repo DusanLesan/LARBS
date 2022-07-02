eab2cb64672105ca209419b133d4b58c01cd1f18
Dusan <oalfodr@gmail.com>
2022-07-02 13:25:03 +0200

    update

--- abs.sh
+++ abs.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 while getopts ":a:r:b:p:h" o; do case "${o}" in
 	h) printf "Optional arguments for custom use:\\n  -r: Dotfiles repository (local file or url)\\n  -p: Dependencies and programs csv (local file or url)\\n  -a: AUR helper (must have pacman-like syntax)\\n  -h: Show this message\\n" && exit 1 ;;
 	r) dotfilesrepo=${OPTARG} && git ls-remote "$dotfilesrepo" || exit 1 ;;
@@ -19,69 +21,67 @@ installpkg(){ pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 ;}
 
 error() { clear; printf "ERROR:\\n%s\\n" "$1" >&2; exit 1;}
 
-getuser() {
-	# Prompts user for new username an password.
-	name=$(dialog --inputbox "First, please enter a name for the user account." 10 60 3>&1 1>&2 2>&3 3>&1) || exit 1
+getuserandpass() {
+	name=$(whiptail --inputbox "First, please enter a name for the user account." 10 60 3>&1 1>&2 2>&3 3>&1) || exit 1
 	while ! echo "$name" | grep -q "^[a-z_][a-z0-9_-]*$"; do
-		name=$(dialog --no-cancel --inputbox "Username not valid. Give a username beginning with a letter, with only lowercase letters, - or _." 10 60 3>&1 1>&2 2>&3 3>&1)
+		name=$(whiptail --nocancel --inputbox "Username not valid. Give a username beginning with a letter, with only lowercase letters, - or _." 10 60 3>&1 1>&2 2>&3 3>&1)
 	done
 	repodir="/home/$name/.local/src"; mkdir -p "$repodir"; chown -R "$name":wheel "$(dirname "$repodir")"
 }
 
-manualinstall() { # Installs $1 manually if not installed. Used only for AUR helper here.
-	[ -f "/usr/bin/$1" ] || (
-	dialog --infobox "Installing \"$1\", an AUR helper..." 4 50
-	cd /tmp || exit 1
-	rm -rf /tmp/"$1"*
-	curl -sO https://aur.archlinux.org/cgit/aur.git/snapshot/"$1".tar.gz &&
-	sudo -u "$name" tar -xvf "$1".tar.gz >/dev/null 2>&1 &&
-	cd "$1" &&
-	sudo -u "$name" makepkg --noconfirm -si >/dev/null 2>&1
-	cd /tmp || return 1) ;
+echo "$* #LARBS" >> /etc/sudoers ;}
+
+manualinstall() {
+	whiptail --infobox "Installing \"$1\", an AUR helper..." 7 50
+	sudo -u "$name" mkdir -p "$repodir/$1"
+	sudo -u "$name" git -C "$repodir" clone --depth 1 --single-branch \
+		--no-tags -q "https://aur.archlinux.org/$1.git" "$repodir/$1" ||
+		{ cd "$repodir/$1" || return 1 ; sudo -u "$name" git pull --force origin master ;}
+	cd "$repodir/$1" || exit 1
+	sudo -u "$name" -D "$repodir/$1" \
+		makepkg --noconfirm -si >/dev/null 2>&1 || return 1
 }
 
-maininstall() { # Installs all needed programs from main repo.
-	dialog --title "LARBS Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 5 70
+maininstall() {
+	whiptail --title "LARBS Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 9 70
 	installpkg "$1"
 }
 
 gitmakeinstall() {
-	progname="$(basename "$1" .git)"
+	progname="${1##*/}"
+	progname="${progname%.git}"
 	dir="$repodir/$progname"
-	dialog --title "LARBS Installation" --infobox "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 5 70
-	sudo -u "$name" git clone --depth 1 "$1" "$dir" >/dev/null 2>&1 || { cd "$dir" || return 1 ; sudo -u "$name" git pull --force origin master;}
+	whiptail --title "LARBS Installation" \
+		--infobox "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 8 70
+	sudo -u "$name" git -C "$repodir" clone --depth 1 --single-branch \
+		--no-tags -q "$1" "$dir" ||
+		{ cd "$dir" || return 1 ; sudo -u "$name" git pull --force origin master ;}
 	cd "$dir" || exit 1
 	make >/dev/null 2>&1
 	make install >/dev/null 2>&1
-	cd /tmp || return 1 ;
+	cd /tmp || return 1
 }
 
 aurinstall() {
-	dialog --title "LARBS Installation" --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 5 70
+	whiptail --title "LARBS Installation" \
+		--infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 9 70
 	echo "$aurinstalled" | grep -q "^$1$" && return 1
 	sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
 }
 
 pipinstall() {
-	dialog --title "LARBS Installation" --infobox "Installing the Python package \`$1\` ($n of $total). $1 $2" 5 70
+	whiptail --title "LARBS Installation" \
+		--infobox "Installing the Python package \`$1\` ($n of $total). $1 $2" 9 70
 	[ -x "$(command -v "pip")" ] || installpkg python-pip >/dev/null 2>&1
 	yes | pip install "$1"
 }
 
 getpackagelist() {
 	([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) || curl -Ls "$progsfile" | tail -n +2 > /tmp/progs.csv
-	if [ $1 -eq 0 ]; then
-		cancel="Turn all off"
-		state="on"
-	else
-		cancel="Turn all on"
-		state="off"
-	fi
+	options=$(awk -F',' '{print $2, "off"}' < /tmp/progs.csv)
+	cmd=(whiptail --checklist --noitem --separate-output --ok-button "Install" --nocancel --title "Select packages to install:" "choose" 40 76 30)
+	 selectedprograms=( $("${cmd[@]}" ${options} 3>&1 1>&2 2>&3) )
 
-	options=$(awk -F',' -v state="$state" '{print $2, state}' < /tmp/progs.csv)
-	cmd=(dialog --stdout --no-items --ok-label "Install" --cancel-label "$cancel" --checklist "Select packages to install:" 50 76 16)
-	selectedprograms=$( ("${cmd[@]}" ${options}) )
-	[ $? -eq 1 ] && getpackagelist $((1-$1))
 }
 
 installationloop() {
@@ -100,13 +100,15 @@ installationloop() {
 	done < /tmp/progs.csv
 }
 
-putgitrepo() { # Downloads a gitrepo $1 and places the files in $2 only overwriting conflicts
-	dialog --infobox "Downloading and installing config files..." 4 60
+putgitrepo() {
+	whiptail --infobox "Downloading and installing config files..." 7 60
 	[ -z "$3" ] && branch="master" || branch="$repobranch"
 	dir=$(mktemp -d)
-	[ ! -d "$2" ] && sudo -u $name mkdir -p "$2"
-	chown -R "$name":wheel "$dir" "$2"
-	sudo -u "$name" git clone --recursive -b "$branch" --depth 1 "$1" "$dir" >/dev/null 2>&1
+	[ ! -d "$2" ] && mkdir -p "$2"
+	chown "$name":wheel "$dir" "$2"
+	sudo -u "$name" git -C "$repodir" clone --depth 1 \
+		--single-branch --no-tags -q --recursive -b "$branch" \
+		--recurse-submodules "$1" "$dir"
 	sudo -u "$name" cp -rfT "$dir" "$2"
 }
 
@@ -119,7 +121,7 @@ compilec() {
 }
 
 systembeepoff() {
-	dialog --infobox "Getting rid of that retarded error beep sound..." 10 50
+	whiptail --infobox "Getting rid of error beep sound..." 10 50
 	rmmod pcspkr
 	echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;
 }
@@ -131,17 +133,18 @@ displaymanager() {
 
 ### THE ACTUAL SCRIPT ###
 
-# Check if user is root on Arch distro. Install dialog.
-pacman --noconfirm --needed -Sy dialog || error "Are you sure you're running this as the root user, are on an Arch-based distribution and have an internet connection?"
+# Check if user is root on Arch distro. Install whiptail.
+pacman --noconfirm --needed -Sy libnewt || error "Are you sure you're running this as the root user, are on an Arch-based distribution and have an internet connection?"
 
-# Get and verify username.
-getuser || error "User exited."
+# Get and verify username and password.
+getuserandpass || error "User exited."
 
 # Allow user to select programs he wants
-getpackagelist 0
+getpackagelist
 
-for x in curl base-devel git ntp zsh; do
-	dialog --title "LARBS Installation" --infobox "Installing \`$x\` which is required to install and configure other programs." 5 70
+for x in curl ca-certificates base-devel git ntp zsh ; do
+	whiptail --title "LARBS Installation" \
+		--infobox "Installing \`$x\` which is required to install and configure other programs." 8 70
 	installpkg "$x"
 done
 
@@ -178,7 +181,3 @@ sudo -u "$name" mkdir -p "/home/$name/.cache/zsh/"
 
 # Start/restart PulseAudio.
 killall pulseaudio; sudo -u pulseaudio --start
-
-# Remove dialog
-pacman -Rcns --noconfirm dialog
-
